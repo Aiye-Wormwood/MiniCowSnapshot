f4cab7b0d36e59b563e51c14375d7c6a0477e0d3
Aiye.Wormwood <wormwood.zhang@gmail.com>
2025-08-16 01:21:21 +0900

    adjustment

--- src/rest_api_run.rs
+++ src/rest_api_run.rs
@@ -1,24 +1,36 @@
 use actix_web::{web, App, HttpServer, HttpResponse};
-use std::sync::{Arc, RwLock};
+use std::collections::HashMap;
 
 // Handler types
-type UpsertFile = actix_web::dev::fn_service_factory<web::Path<String>, String, HttpResponse>;
-type GetFile = actix_web::dev::fn_service_factory<web::Path<String>, HttpResponse>;
-type CreateSnapshot = actix_web::dev::fn_service_factory<(), HttpResponse>;
-type RestoreSnapshot = actix_web::dev::fn_service_factory<web::Path<String>, HttpResponse>;
+type UpsertFile = fn(web::Path<String>, String, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture;
+type GetFile = fn(web::Path<String>, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture;
+type CreateSnapshot = fn(web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture;
+type RestoreSnapshot = fn(web::Path<String>, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture;
 
 pub async fn run_rest_api(
-    upsert_file: impl Fn(web::Path<String>, String) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
-    get_file: impl Fn(web::Path<String>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
-    create_snapshot: impl Fn() -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
-    restore_snapshot: impl Fn(web::Path<String>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
+    upsert_file: impl Fn(web::Path<String>, String, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
+    get_file: impl Fn(web::Path<String>, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
+    create_snapshot: impl Fn(web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
+    restore_snapshot: impl Fn(web::Path<String>, web::Query<HashMap<String, String>>) -> actix_web::dev::HttpResponseFuture + Clone + Send + 'static,
 ) -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
-            .route("/file/{name}", web::put().to(upsert_file.clone()))
-            .route("/file/{name}", web::get().to(get_file.clone()))
-            .route("/snapshot", web::post().to(create_snapshot.clone()))
-            .route("/snapshot/{id}/restore", web::post().to(restore_snapshot.clone()))
+            .route("/file/{name}", web::put().to({
+                let upsert_file = upsert_file.clone();
+                move |path, body, query| upsert_file(path, body, query)
+            }))
+            .route("/file/{name}", web::get().to({
+                let get_file = get_file.clone();
+                move |path, query| get_file(path, query)
+            }))
+            .route("/snapshot", web::post().to({
+                let create_snapshot = create_snapshot.clone();
+                move |query| create_snapshot(query)
+            }))
+            .route("/snapshot/{id}/restore", web::post().to({
+                let restore_snapshot = restore_snapshot.clone();
+                move |path, query| restore_snapshot(path, query)
+            }))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
